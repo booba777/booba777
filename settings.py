fbeab44391f9026229588e4d493505a9f37d10b2
Саня <booba777.shop@gmail.com>
2025-03-11 16:14:59 +0300

    Initial project setup

--- settings.py
+++ settings.py
@@ -1,2 +1,7 @@
+nano website/settings.py
+# Вставте код з артефакту
+
+# Похоже, что строка `booba777` была вставлена по ошибке. Удалим её.
+
 nano website/booba777/settings.py
 # Вставте код з артефакту
